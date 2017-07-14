c5d0b52846d7d33446122dc1bef7801f0e97b771
Betty Seifu <betty.seifu149@gmail.com>
2017-07-14 15:59:02 +0300

    Zero issue handeled.

--- src/Calculator.java
+++ src/Calculator.java
@@ -19,25 +19,19 @@ public class Calculator {
 
         System.out.println(a/b);
     }
-
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter 2 numbers:");
         System.out.println("First number:");
         int first=sc.nextInt();
-
         System.out.println("Second number:");
         int second=sc.nextInt();
-
-
         while (second==0){
             System.out.println("Can not divide. Enter again:");
         second=sc.nextInt();}
         add(first,second);
         sub(first,second);
         mul(first,second);
-
         div(first,second);
 
 
