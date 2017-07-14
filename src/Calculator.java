c1b56ef220c966fe78b06fbceccb69445dd20c53
Betty Seifu <betty.seifu149@gmail.com>
2017-07-14 14:37:55 +0300

    Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Created by betty on 7/13/17.
+ */
+
+public class Calculator {
+    public static void add(int a,int b){
+        System.out.println((a+b));
+
+    }
+    public static void sub(int a,int b){
+        System.out.println(a-b);
+    }
+    public static void mul(int a,int b){
+        System.out.println(a*b);
+    }
+    public static void div(int a,int b){
+        System.out.println(a/b);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 numbers:");
+        System.out.println("First number:");
+        int first=sc.nextInt();
+        System.out.println("Second number:");
+        int second=sc.nextInt();
+        add(first,second);
+        sub(first,second);
+        mul(first,second);
+        div(first,second);
+
+
+
+    }
+}
